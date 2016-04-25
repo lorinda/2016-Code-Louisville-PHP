1f3ae12e90a00b4afe9064fbc21770336f31c8ce
Lorinda <lorinda@users.noreply.github.com>
2016-04-25 13:23:37 -0400

    Added email submission code

--- myform.php
+++ myform.php
@@ -1,25 +1,73 @@
+<!--Include navigation menu-->
 <?php include("includes/header.php"); ?>
+
+<!-- Code for form submission -->
 <?php
-$name = htmlspecialchars($_POST['name']);
-$email = htmlspecialchars($_POST['email']);
-$subject = htmlspecialchars($_POST['subject']);
-$message = htmlspecialchars($_POST['message']);    
+//Check for SQL Injection
+$name = check_input($_POST['name'],"Enter your name!");
+$email = check_input($_POST['email']);
+$subject = check_input($_POST['subject']);
+$message = check_input($_POST['message']);
+$phone = check_input($_POST['phone']);
+
+if (preg_match("/\D/",$phone))
+{
+    die("Please enter numbers only for Phone #");
+}
+
+$recipient = "lorinda.peoples@yahoo.com";
+
+//Email Message
+$email_from = $email;
+$email_subject = "Website Contact: ".$subject;
+$email_body = $name." filled out the contact form on www.stokesandglass.com.\n".
+                "\nHere is the message:\n\n".$message.
+                "\n\nFrom: ".$name."\nEmail: ".$email_from.
+                "\nPhone Number: ".$phone.
+                "\n--End of Message--";
+
+
+$headers ="From: lstokes@gator4025.hostgator.com"."\r\n". 
+    "Reply-To: ".$email_from."\r\n";
+
+//Send Email
+mail($recipient, $email_subject, $email_body, $headers);
 ?>
 
-<div>
-    <br>
-    <br>
-    <br>
-        Your name is: <?php echo $_POST['name']; ?><br />
+<html>
+<!--Message to display on the redirected site after submit-->
+    <body>
+        Your message was successfully sent!<br />
+        Thank you for contacting us! <br />
+        <a href="http://www.stokesandglass.com">Back to Site</a><br />
+        <br />Email Contents: <br />
+        Your name: <?php echo $_POST['name']; ?><br />
         Your e-mail: <?php echo $_POST['email']; ?><br />
+        Your phone: <?php echo $_POST['phone']; ?><br />
         <br />
         Subject:<br />
         <?php echo $_POST['subject']; ?>
         <br />
         Message:<br />
         <?php echo $_POST['message']; ?>
-</div>
+    </body>
+</html>
 
+
+
+<?php
+//function to check for SQL injection
+function check_input($data,$problem='')
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    if ($problem && strlen($data) == 0)
+    {
+        die($problem);
+    }return $data;
+}
+?>
 <!--Code for "Contact Us" Footer-->
 			<footer id="footer">
 			
